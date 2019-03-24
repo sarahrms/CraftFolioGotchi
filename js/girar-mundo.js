8eebd503a1304e6609683eaaa741c4c073442820
JuGP <julianegp@hotmail.com>
2019-03-23 21:23:13 -0300

    Arrumando mundo

--- js/girar-mundo.js
+++ js/girar-mundo.js
@@ -1,14 +1,37 @@
 document.addEventListener("DOMContentLoaded", function(e) {
 	var ir_esquerda = document.querySelector("#seta-esquerda");
   var ir_direita = document.querySelector("#seta-direita");
-  var faces = document.querySelector("#faces");
+  var faces = document.querySelector(".box-faces");
+	var localizacao = document.querySelector("#localizacao");
 
   ir_esquerda.addEventListener('click', function(e){
-    faces.classList.add("girar-esquerda");
+		console.log("entrou");
+		if(localizacao.innerHTML=="DIREITO"){
+				faces.style.animationName="spin-direito";
+				faces.style.animationDirection="reverse";
+				localizacao.innerHTML="CENTRO";
+		}else{
+			faces.style.animationName="spin-esquerdo";
+			faces.style.animationDirection="normal";
+			localizacao.innerHTML="ESQUERDA";
+		}
+		faces.style.animationDuration="5s";
+		faces.style.animationFillMode="forwards";
   });
 
   ir_direita.addEventListener('click', function(e){
-    faces.classList.add("girar-direita");
+		if(localizacao.innerHTML=="ESQUERDA"){
+			faces.style.animationName="spin-esquerdo";
+			faces.style.animationDirection="reverse";
+			localizacao.innerHTML="CENTRO";
+		}else{
+			faces.style.animationName="spin-direito";
+			faces.style.animationDirection="normal";
+			localizacao.innerHTML="DIREITO";
+		}
+		faces.style.animationDuration="5s";
+		faces.style.animationFillMode="forwards";
+
   });
 
 });
--- js/modo-edicao.js
+++ js/modo-edicao.js
@@ -1,13 +1,13 @@
 document.addEventListener("DOMContentLoaded", function(e) {
 	var botao_modo = document.querySelector("#modo-exibicao");
-  var mundo = document.querySelector("#mundo");
+  var mundo = document.querySelector(".page");
 
   botao_modo.addEventListener('click', function(e){
     if(botao_modo.innerHTML == "Modo Edição"){
-      mundo.style.left = "25%";
+      mundo.style.marginLeft = "50%";
       botao_modo.innerHTML = "Modo Visualização";
     }else{
-      mundo.style.left = "0%";
+      mundo.style.marginLeft = "0%";
       botao_modo.innerHTML = "Modo Edição";
     }
 
