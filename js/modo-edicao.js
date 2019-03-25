dd06be5f372b045a3546e9a5053bd51b6889019d
JuGP <julianegp@hotmail.com>
2019-03-25 19:40:16 -0300

    Incluindo localStorage para posição das widgets

--- js/modo-edicao.js
+++ js/modo-edicao.js
@@ -4,10 +4,24 @@ var botao_modo = document.querySelector("#modo-exibicao");
   var widgets = document.querySelectorAll(".widgets");
   var dragDrop = document.querySelectorAll(".drag-drop");
 
+	for (var i = 0; i < dragDrop.length; i++) {
+		var aux_transform = localStorage.getItem("dragDrop_"+i+"_transform");
+		var aux_classList = localStorage.getItem("dragDrop_"+i+"_classList");
+		if (aux_transform!=null) {
+			dragDrop[i].style.transform = aux_transform;
+		}
+		if (aux_classList!=null) {
+			dragDrop[i].classList = aux_classList;
+		}
+
+	}
+
   botao_modo.addEventListener('click', function(e){
     if(botao_modo.innerHTML == "Modo Edição"){
       botao_modo.innerHTML = "Modo Visualização";
 		for (var i = 0; i < dragDrop.length; i++) {
+			localStorage.setItem("dragDrop_"+i+"_transform",dragDrop[i].style.transform);
+			localStorage.setItem("dragDrop_"+i+"_classList",dragDrop[i].classList);
 			if (!dragDrop[i].classList.contains("exibir")) {
 				dragDrop[i].style.display = "none";
 			}
