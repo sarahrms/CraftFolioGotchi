2f8d58ef591171dab1d7d4f107131d41f86d51fd
JuGP <julianegp@hotmail.com>
2019-03-25 20:09:57 -0300

    Adicionando funcionalidade de alterar o tamanho das widgets

--- js/arrastar-soltar.js
+++ js/arrastar-soltar.js
@@ -45,8 +45,46 @@ interact('.drag-drop')
     autoScroll: true,
     // dragMoveListener from the dragging demo above
     onmove: dragMoveListener
-  });
+  })
+  .resizable({
+    // resize from all edges and corners
+    edges: { left: true, right: true, bottom: true, top: true },
 
+    modifiers: [
+      // keep the edges inside the parent
+      interact.modifiers.restrictEdges({
+        outer: 'parent',
+        endOnly: true,
+      }),
+
+      // minimum size
+      interact.modifiers.restrictSize({
+        min: { width: 100, height: 50 },
+      }),
+    ],
+
+    inertia: true
+  })
+  .on('resizemove', function (event) {
+    var target = event.target,
+        x = (parseFloat(target.getAttribute('data-x')) || 0),
+        y = (parseFloat(target.getAttribute('data-y')) || 0);
+
+    // update the element's style
+    target.style.width  = event.rect.width + 'px';
+    target.style.height = event.rect.height + 'px';
+
+    // translate when resizing from top or left edges
+    x += event.deltaRect.left;
+    y += event.deltaRect.top;
+
+    target.style.webkitTransform = target.style.transform =
+        'translate(' + x + 'px,' + y + 'px)';
+
+    target.setAttribute('data-x', x);
+    target.setAttribute('data-y', y);
+    target.textContent = Math.round(event.rect.width) + '\u00D7' + Math.round(event.rect.height);
+  });
   function dragMoveListener (event) {
     var target = event.target,
         // keep the dragged position in the data-x/data-y attributes
@@ -62,4 +100,3 @@ interact('.drag-drop')
     target.setAttribute('data-x', x);
     target.setAttribute('data-y', y);
   }
-
--- js/modo-edicao.js
+++ js/modo-edicao.js
@@ -7,12 +7,20 @@ var botao_modo = document.querySelector("#modo-exibicao");
 	for (var i = 0; i < dragDrop.length; i++) {
 		var aux_transform = localStorage.getItem("dragDrop_"+i+"_transform");
 		var aux_classList = localStorage.getItem("dragDrop_"+i+"_classList");
+		var aux_height = localStorage.getItem("dragDrop_"+i+"_height");
+		var aux_width = localStorage.getItem("dragDrop_"+i+"_width");
 		if (aux_transform!=null) {
 			dragDrop[i].style.transform = aux_transform;
 		}
 		if (aux_classList!=null) {
 			dragDrop[i].classList = aux_classList;
 		}
+    if (aux_height!=null) {
+			dragDrop[i].style.height = aux_height;
+		}
+    if (aux_width!=null) {
+      dragDrop[i].style.width = aux_width;
+    }
 
 	}
 
@@ -22,6 +30,9 @@ var botao_modo = document.querySelector("#modo-exibicao");
 		for (var i = 0; i < dragDrop.length; i++) {
 			localStorage.setItem("dragDrop_"+i+"_transform",dragDrop[i].style.transform);
 			localStorage.setItem("dragDrop_"+i+"_classList",dragDrop[i].classList);
+			localStorage.setItem("dragDrop_"+i+"_height",dragDrop[i].style.height);
+			localStorage.setItem("dragDrop_"+i+"_width",dragDrop[i].style.width);
+      
 			if (!dragDrop[i].classList.contains("exibir")) {
 				dragDrop[i].style.display = "none";
 			}
@@ -31,7 +42,7 @@ var botao_modo = document.querySelector("#modo-exibicao");
     }else{
       botao_modo.innerHTML = "Modo Edição";
 		for (var i = 0; i < dragDrop.length; i++) {
-				dragDrop[i].style.display = "block";			
+				dragDrop[i].style.display = "block";
 		}
 		widgets[0].style.backgroundColor = "cornflowerblue"
 		widgets[1].style.backgroundColor = "cornflowerblue"
