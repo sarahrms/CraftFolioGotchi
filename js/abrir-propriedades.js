8e0411aa1df93caf9a3d84f00e9e12e384fe33ef
Juliane Guedes <julianegp@hotmail.com>
2019-03-29 18:30:52 -0300

    Incrementando parte das propriedades

--- js/abrir-propriedades.js
+++ js/abrir-propriedades.js
@@ -2,6 +2,7 @@ document.addEventListener("DOMContentLoaded", function(e) {
   var dragDrop = document.querySelectorAll(".drag-drop");
   var propriedades = document.querySelector("#propriedades");
   var dropzone = document.querySelector("#inner-dropzone");
+  var botao = document.querySelectorAll(".botao-propriedades");
 
 	for (var i = 0; i < dragDrop.length; i++) {
       	dragDrop[i].addEventListener('click', function(e){
@@ -9,13 +10,14 @@ document.addEventListener("DOMContentLoaded", function(e) {
 		    }else if (this.classList.contains("imagem")) {
 
 		    	propriedades.innerHTML='<form>'+
+		    	'<input type="hidden" name="id" value="'+this.id+'">'+
 				'<label for="url" >URL:</label>'+
 				'<input id="url" type="text"  name="url">'+
 				'<label for="altura">Altura:</label>'+
 				'<input id="altura" type="password" name="altura">'+
 				'<label for="largura">Largura:</label>'+
 				'<input id="largura" type="password" name="largura">'+
-				'<button type="submit" >Alterar</button>'+
+				'<button type="submit" class="botao-propriedades">Alterar</button>'+
 				'</form>';
 		        
 		    }else if (this.classList.contains("video")) {
@@ -26,6 +28,15 @@ document.addEventListener("DOMContentLoaded", function(e) {
 
       	});
 
+	}
+
+	for (var i = 0; i < botao.length; i++) {
+      	botao[i].addEventListener('click', function(e){
+	    	var id = document.querySelector("#propriedades > form > input[type='hidden']:nth-child(1)").value;
+	    	var objeto =  document.querySelector("#"+id);
+	    	console.log(id);
+
+      	});
 	}
 
 	dropzone.addEventListener('click', function(e){
